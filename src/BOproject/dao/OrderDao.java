05c1c012c98f92f25a5430a4ce6ed49da2b45d8a
HeewonLEE2 <gmldnjs1616@gmail.com>
2025-07-30 17:55:34 +0900

    order파일 80%완료

--- src/BOproject/dao/OrderDao.java
+++ src/BOproject/dao/OrderDao.java
@@ -1,5 +1,36 @@
 package BOproject.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import BOproject.model.OrderVO;
+import BOproject.model.UserVO;
+
 public interface OrderDao {
 
+	public abstract List<OrderVO> listOrder() throws SQLException;
+	
+	public abstract OrderVO getOrder(int oid) throws SQLException;
+	
+	public abstract int registOrder(OrderVO order) throws SQLException;
+	
+	public abstract int modifyOrder(OrderVO order) throws SQLException;
+	
+	public abstract int removeOrder(int oid) throws SQLException;
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/BOproject/service/OrderService.java
+++ src/BOproject/service/OrderService.java
@@ -1,5 +1,19 @@
 package BOproject.service;
 
-public interface OrderService {
+import java.sql.SQLException;
+import java.util.List;
 
+import BOproject.model.OrderVO;
+
+public interface OrderService {
+	
+	public abstract List<OrderVO> listOrder() throws SQLException;
+	
+	public abstract OrderVO getOrder(int oid) throws SQLException;
+	
+	public abstract int registOrder(OrderVO order) throws SQLException;
+	
+	public abstract int modifyOrder(OrderVO order) throws SQLException;
+	
+	public abstract int removeOrder(int oid) throws SQLException;
 }
