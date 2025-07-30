eb6c5111c5df94873fb4c08eb5b606d9f2f15500
HeewonLEE2 <gmldnjs1616@gmail.com>
2025-07-30 16:43:23 +0900

    user,product service 구현 완료, test완료

--- src/BOproject/Main/UserTestMain.java
+++ src/BOproject/Main/UserTestMain.java
@@ -2,11 +2,13 @@ package BOproject.Main;
 
 import java.sql.SQLException;
 
-import BOproject.model.UserVO;
+import BOproject.model.ProductVO;
+import BOproject.service.ProductService;
 import BOproject.service.UserService;
+import BOproject.service.impl.ProductServiceImpl;
 import BOproject.service.impl.UserServiceImpl;
 
-public class Main {
+public class UserTestMain {
 
 	public static void main(String[] args) {
 		
@@ -15,12 +17,21 @@ public class Main {
 		try {
 			
 		userService.listUser().stream().forEach(System.out::println);
+		//System.out.println(userService.getUser("gmldnjs1616@hanmail.net"));
 		//userService.registUser(new UserVO("pratice16@hanmail.net", "33희원", "as33333","서울특별시 강남구", "010-1221-1111"));
 		//userService.modifyUser(new UserVO("pratice16@hanmail.net", "수정33희원", "수정as33333","서울특별시 강남구", "010-1221-1111"));
-		userService.removeUser("pratice16@hanmail.net");
+		//userService.removeUser("pratice16@hanmail.net");
 		
+			
 		}catch(SQLException sqle) {
 			sqle.printStackTrace();
 		}
 	}
 }
+
+
+
+
+
+
+
